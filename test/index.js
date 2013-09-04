6eff1294182d9a6266179a0a0dd3770fa1074dad
Naoyuki Kanezawa <naoyuki.kanezawa@gmail.com>
2013-09-05 03:10:05 +0900

    support allowHalfOpen=false and add tests

--- test/index.js
+++ test/index.js
@@ -32,37 +32,84 @@ describe('socket.io-stream', function() {
   });
 
   describe('clean up', function() {
-    it('should clean up local streams on error', function() {
-      var socket = ss(client())
-        , stream = ss.createStream();
-
-      function streams() {
-        return Object.keys(socket.streams);
-      }
-
-      stream = ss.createStream();
-      socket.emit('foo', stream);
-      expect(streams().length).to.eql(1);
-      stream.emit('error', new Error());
-      expect(streams().length).to.eql(0);
+    beforeEach(function() {
+      this.socket = ss(client());
+      this.streams = function() {
+        return Object.keys(this.socket.streams);
+      };
     });
 
-    it('should clean up remote streams on error', function() {
-      var socket = ss(client())
-        , stream = ss.createStream();
-
-      function streams() {
-        return Object.keys(socket.streams);
-      }
-
-      socket.on('foo', function(stream) {
-        expect(streams().length).to.eql(1);
-        stream.emit('error', new Error());
-        expect(streams().length).to.eql(0);
+    describe('local streams', function() {
+      beforeEach(function() {
+        this.stream = ss.createStream();
+        this.socket.emit('foo', this.stream);
+        expect(this.streams().length).to.eql(1);
       });
 
-      // emit a new stream event manually.
-      socket._onstream([0], 'foo', 1);
+      it('should clean up on error', function() {
+        this.stream.emit('error', new Error());
+        expect(this.streams().length).to.eql(0);
+      });
+
+      it('should clean up on finish', function(done) {
+        var self = this;
+        // clean up is done on nextTick.
+        this.stream.emit('finish');
+        this.stream.on('end', function() {
+          expect(self.streams().length).to.eql(0);
+          done();
+        });
+      });
+
+      it('should clean up on end', function() {
+        this.stream.emit('end');
+        expect(this.streams().length).to.eql(0);
+      });
+    });
+
+    describe('remote streams', function() {
+      beforeEach(function(done) {
+        var self = this;
+        this.socket.on('foo', function(stream) {
+          expect(self.streams().length).to.eql(1);
+          self.stream = stream;
+          done();
+        });
+        // emit a new stream event manually.
+        this.socket._onstream([0], 'foo', 1);
+      });
+
+      it('should clean up on error', function() {
+        this.stream.emit('error', new Error());
+        expect(this.streams().length).to.eql(0);
+      });
+
+      it('should clean up on finish', function(done) {
+        var self = this;
+        // clean up is done on nextTick.
+        this.stream.emit('finish');
+        this.stream.on('end', function() {
+          expect(self.streams().length).to.eql(0);
+          done();
+        });
+      });
+
+      it('should clean up on end', function() {
+        this.stream.emit('end');
+        expect(this.streams().length).to.eql(0);
+      });
+    });
+
+    it('should clean up after both "finish" and "end" were called when allowHalfOpen is enabled', function() {
+      var stream = ss.createStream({allowHalfOpen: true});
+      this.socket.emit('foo', stream);
+      expect(this.streams().length).to.eql(1);
+
+      stream.emit('end');
+      expect(this.streams().length).to.eql(1);
+
+      stream.emit('finish');
+      expect(this.streams().length).to.eql(0);
     });
   });
 });
